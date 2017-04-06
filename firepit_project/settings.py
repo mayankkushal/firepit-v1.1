b7368366e3d688ecad663eba14afbcdaa627932f
mayankkushal <mayankkushal26@gmail.com>
2017-04-07 02:15:47 +0530

    s

--- firepit_project/settings.py
+++ firepit_project/settings.py
@@ -282,6 +282,7 @@ import dj_database_url
 DATABASES = { 'default': dj_database_url.config() }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['*']
 
